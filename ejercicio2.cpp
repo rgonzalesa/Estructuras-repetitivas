971059b0f96ce9d69f81f938b7d89a3f89136b4b
RodrigoGonzalesAlanoca <rgonzalesa@unjbg.edu.pe>
2026-07-17 22:38:34 -0500

    Actualiza ejercicio 2 de estructuras repetitivas: agrega validacion de entrada

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -6,6 +6,11 @@ int main() {
     cout << "Ingrese un numero N: ";
     cin >> n;
 
+    if (n < 0) {
+        cout << "Error: no se permiten numeros negativos" << endl;
+        return 1;
+    }
+
     while (i <= n) {
         suma += i;
         i++;
